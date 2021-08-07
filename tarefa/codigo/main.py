92e9310841613d69198dcb45d960dbb5789b0a4c
Ricardo Marra <ricardomarraa@gmail.com>
2021-08-07 00:40:01 -0300

    API funcionando!

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,27 @@
 import pickle
-from fastapi import FastAPI
+import pandas as pd
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int, Age:float, Lifeboat:int, Pclass:int, response:Response):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
 
-@app.get('/model')
-def get():
-    return {'hello':'test'}
+    x_dict = {
+                'sex': [Sex],
+                'age': [Age],
+                'lifeboat': [Lifeboat],
+                'pclass': [Pclass]
+            }
+    
+    X = pd.DataFrame(x_dict)
+    results = float(titanic.predict(X)[0])
+
+    survived = True if results == 0 else False
+    response.status_code = status.HTTP_200_OK
+
+    return {'survived': survived,
+            'status': response.status_code,
+            'message': 'SUCCESS'}
